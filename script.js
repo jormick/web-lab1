944959dd356ed4040cc0d9d41f350f4d6f8feeb3
Vlad Kozlov <65humuge@gmail.com>
2024-09-21 11:18:37 +0300

    main coomit. big commit

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ function addAnimation() {
         });
     });
 }
+
+const randomTitles = [
+    "Strawberry fields are too dangerous!",
+    "iOS 18 is here! What's new?",
+    "AI is going to take over the world!",
+    "Why Fortnite is so popular?",
+    "The best way to make money online",
+    "Did Ryan Gosling die in the end of Drive?",
+    "Chrome, Edge, Arc or Firefox: what is the best browser on Windows?",
+    "Who is Alexander L. and why he hates fishes?",
+];
